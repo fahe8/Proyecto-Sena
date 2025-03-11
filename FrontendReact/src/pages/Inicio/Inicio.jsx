74b8d6939e59fee26c02a946a9d24efd843c778c
Fabian <fabiancarrion1@gmail.com>
2025-03-11 10:13:45 -0500

    evitar propagacion de onclick en slider

--- FrontendReact/src/pages/Inicio/Inicio.jsx
+++ FrontendReact/src/pages/Inicio/Inicio.jsx
@@ -36,13 +36,49 @@ const Inicio = () => {
 const ListaEmpresas = () => {
   const navigate = useNavigate();
 
+  const CustomPrevArrow = (props) => {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", left: "-25px" }}
+        onClick={(event) => {
+          event.stopPropagation();
+          onClick();
+        }}
+      />
+    );
+  };
+  
+  const CustomNextArrow = (props) => {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", right: "-25px" }}
+        onClick={(event) => {
+          event.stopPropagation();
+          onClick();
+        }}
+      />
+    );
+  };
+  
+
+
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    nextArrow: <CustomNextArrow />,
+    prevArrow: <CustomPrevArrow />,
   };
+
+
+
+
   return (
     <div
       className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 justify-self-center"
